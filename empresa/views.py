c1d0e26302435862a31ae81f221e0eaf98553884
rafaela <rafaela20augusto@gmail.com>
2022-11-19 16:42:22 -0300

    Add improvements and visual changes

--- empresa/views.py
+++ empresa/views.py
@@ -18,10 +18,11 @@ def nova_empresa(request):
         tecnologias = request.POST.getlist('tecnologias')
         logo = request.FILES.get('logo')
 
-        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or len(caracteristicas.strip()) == 0 or (not logo)): 
+        if (len(nome.strip()) == 0 or len(email.strip()) == 0 or len(cidade.strip()) == 0 or len(endereco.strip()) == 0 or len(nicho.strip()) == 0 or (not logo)): 
             messages.add_message(request, constants.ERROR, 'Preencha todos os campos.')
             return redirect('/home/nova_empresa')
 
+        
         if logo.size > 100_000_000:
             messages.add_message(request, constants.ERROR, 'A logo da empresa deve ter menos de 10MB.')
             return redirect('/home/nova_empresa')
